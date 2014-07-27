1223e43f864bd7fff6e35ecf83d3f699c71a9eca
Saurabh <sau.sri@gmail.com>
2014-07-27 13:55:46 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function makeCacheMatrix stores tbe inverse of matrix in global variable
 
-## Write a short comment describing this function
+
+## makeCacheMatrix stores the inverser in a global variable and creates vector with specific functions as list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mi <- NULL
+        set <- function(y) {
+                x <<- y
+                mi <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inverse) mi <<- inverse
+        getInv <- function() mi
+        list(set = set, get = get,
+                setInv = setInv,
+                getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks if the inverser matrix already exists, if found returns the change matrix else calculates the invers
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        mi <- x$getInv()
+        if(!is.null(mi)) {
+                message("getting cached data")
+                return(mi)
+        }
+        data <- x$get()
+        mi <- solve(data)
+        x$setInv(mi)
+        mi
 }
 
